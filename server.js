2e4b00aea06eb9d46795810556474aca1565095e
khoury smith <khourysmith@khourys-MacBook-Pro.local>
2020-05-06 20:58:46 -0400

    testing heroku one more time

--- server.js
+++ server.js
@@ -58,45 +58,45 @@ app.post('/dates/', (req, res) => {
     });
 });
 
-// app.get('/seed', async (req, res) => {
-//     const newDates =
-//     [
-//     {
-//         name: 'Escape Room Date Night',
-//         description: 'The timer starts ticking down from sixty minutes and you immediately feel an adrenaline rush. Will you make it out of this escape room before the clock reaches zero? You rush around and begin to make sense of hidden clues and riddles.',
-//         type: ['Cooperative', 'Active', 'Adventure'], 
-//         address: [{
-//             'streetNumber': 209,
-//             'street': 'S Olive Ave',
-//             'city': 'West Palm Beach',
-//             'state': 'Florida',
-//             'zip': 33401
-//         }],
-//         phoneNumber: 5616695120,
-//         img:'https://worldofescapes-com.s3.amazonaws.com/uploads/quests/10890/large/the-escape-game-san-francisco-gold-rush.jpg'
-//     }, {
-//             name: 'Performers Playground',
-//             description: 'Performers Playground Is A Specialized Boutique Performing Arts Studio With An Exciting After School Program. We Provide Top Notch Study In The Areas Of Dance, Theatre and Voice And Our After School Program Immerses Our Students In All Three With Fun Extras Like Art And Yoga. ',
-//             type: ['Performing Arts', 'Dance'], 
-//             address: [{
-//                 'streetNumber': 12900,
-//                 'street': 'West State Road 84',
-//                 'city': 'Ft Lauderdale',
-//                 'state': 'Florida',
-//                 'zip': 33325
-//             }],
-//             phoneNumber: 9543688534,
-//             img: 'http://performersplayground.com/wp-content/uploads/2019/01/211.jpg'
-//         }
-//     ]
+app.get('/seed', async (req, res) => {
+    const newDates =
+    [
+    {
+        name: 'Escape Room Date Night',
+        description: 'The timer starts ticking down from sixty minutes and you immediately feel an adrenaline rush. Will you make it out of this escape room before the clock reaches zero? You rush around and begin to make sense of hidden clues and riddles.',
+        type: ['Cooperative', 'Active', 'Adventure'], 
+        address: [{
+            'streetNumber': 209,
+            'street': 'S Olive Ave',
+            'city': 'West Palm Beach',
+            'state': 'Florida',
+            'zip': 33401
+        }],
+        phoneNumber: 5616695120,
+        img:'https://worldofescapes-com.s3.amazonaws.com/uploads/quests/10890/large/the-escape-game-san-francisco-gold-rush.jpg'
+    }, {
+            name: 'Performers Playground',
+            description: 'Performers Playground Is A Specialized Boutique Performing Arts Studio With An Exciting After School Program. We Provide Top Notch Study In The Areas Of Dance, Theatre and Voice And Our After School Program Immerses Our Students In All Three With Fun Extras Like Art And Yoga. ',
+            type: ['Performing Arts', 'Dance'], 
+            address: [{
+                'streetNumber': 12900,
+                'street': 'West State Road 84',
+                'city': 'Ft Lauderdale',
+                'state': 'Florida',
+                'zip': 33325
+            }],
+            phoneNumber: 9543688534,
+            img: 'http://performersplayground.com/wp-content/uploads/2019/01/211.jpg'
+        }
+    ]
 
-// try {
-//     const seedItems = await DateI.create(newDates)
-//     res.send(seedItems)
-// } catch (err) {
-//     res.send(err.message)
-// }
-// })
+try {
+    const seedItems = await DateI.create(newDates)
+    res.send(seedItems)
+} catch (err) {
+    res.send(err.message)
+}
+})
 
 
 
@@ -142,6 +142,6 @@ app.put('/dates/:id', (req, res) => {
 
 
 // listen
-app.listen(3000, () => {
-    console.log('listening on: ' + 3000);
+app.listen(PORT, () => {
+    console.log('listening on: ' + PORT);
 });
